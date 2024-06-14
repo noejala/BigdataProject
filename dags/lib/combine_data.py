9dc9d9431deb57a3f8bb1a711adb6ff4d31d5b90
noejala <119047126+noejala@users.noreply.github.com>
2024-06-14 15:11:46 +0200

    modifs

--- dags/lib/combine_data.py
+++ dags/lib/combine_data.py
@@ -2,40 +2,39 @@ import os
 from pyspark.sql import SparkSession
 
 
-
+#Combine entre les 2 API
 def formatting_combine():
     def merge_parquet_files():
-        # Initialiser une session Spark
+        # Session Spark pour traiter les données
         spark = SparkSession.builder \
             .appName("Combine Belib' Data") \
             .getOrCreate()
 
-        # Définir le chemin de base à partir du script actuel pour remonter jusqu'à la racine du projet
+        # Définition du path
         project_root = os.path.abspath(os.path.join('../..'))
 
-        # Chemins des fichiers Parquet d'entrée
+        # Fichiers parquet d'entrée
         static_data_path = os.path.join(project_root, 'data/formatted/belibstaticdata')
         realtime_data_path = os.path.join(project_root,'data/formatted/belibrealtime')
 
-        # Lire les fichiers Parquet
+        # Lecture des fichiers parquet
         static_df = spark.read.parquet(static_data_path)
         realtime_df = spark.read.parquet(realtime_data_path)
 
-        # Afficher les colonnes pour chaque DataFrame
         print("Static DataFrame Columns:", static_df.columns)
         print("Realtime DataFrame Columns:", realtime_df.columns)
 
-        # Fusionner les DataFrames sur les colonnes 'id_pdc_local' et 'id_pdc'
+        # Join sur 'id_pdc_local' et 'id_pdc'
         merged_df = static_df.join(realtime_df, static_df.id_pdc_local == realtime_df.id_pdc, how='inner')
 
-        # Chemin du fichier Parquet de sortie
+        # Fichier parquet de sortie
         output_dir = os.path.join(project_root, 'data')
         output_path = os.path.join(output_dir, 'usage')
 
-        # Créer le répertoire de sortie s'il n'existe pas
+        # Verifictaion que le répértoire de sortie existe
         os.makedirs(output_dir, exist_ok=True)
 
-        # Écrire le DataFrame fusionné en Parquet avec l'option 'overwrite'
+        # Overwrite
         merged_df.write.mode('overwrite').parquet(output_path)
 
         print(f"Data successfully merged and written to Parquet file at {output_path}")
